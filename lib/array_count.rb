322b90cc7e0919b86a944274239f7dea5a0461ad
Fem Adisa <femiwonderful@gmail.com>
2020-11-19 23:59:42 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,3 +5,19 @@ end
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
+
+sit = [1,3,3,4, 6]
+sit.count do |element|
+  puts element
+end
+
+for a in sit
+  puts a
+end
+puts "\n"
+
+sit.count do |element|
+  element.even?
+end
+#puts src
+puts sit.count
