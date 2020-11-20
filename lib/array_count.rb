af6a6c96a48d88b12425a4395750930eb9dbc6ad
Fem Adisa <femiwonderful@gmail.com>
2020-11-20 00:38:52 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,16 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  array.count do |i|
+    i.class == String 
+  end
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
 
+puts count_strings([1,"1", 2, "2", 3])
+puts "\n"
 sit = [1,0,3, -4,3,4, 6]
 soup = sit.count do |element|
   print element
